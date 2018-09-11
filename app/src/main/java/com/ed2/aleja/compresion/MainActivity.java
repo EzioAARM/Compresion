7484d95842cbcb0790bad7044914eb5594724249
EzioAARM <alejandrom9712@gmail.com>
2018-09-11 12:20:30 -0600

    Actualizacion de diseño

--- app/src/main/java/com/ed2/aleja/compresion/MainActivity.java
+++ app/src/main/java/com/ed2/aleja/compresion/MainActivity.java
@@ -1,11 +1,14 @@
 package com.ed2.aleja.compresion;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_navigation);
         setSupportActionBar(toolbar);
 
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.nav_open, R.string.nav_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            // Cierra el activity Drawer
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            // Otra accion cuando se presione regresar
+            super.onBackPressed();
+        }
     }
 }

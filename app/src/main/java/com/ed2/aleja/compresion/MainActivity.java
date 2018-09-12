8e6f2e90966847dc573970514e4a54435de810b8
EzioAARM <alejandrom9712@gmail.com>
2018-09-11 21:56:39 -0600

    Navigation Drawer

--- app/src/main/java/com/ed2/aleja/compresion/MainActivity.java
+++ app/src/main/java/com/ed2/aleja/compresion/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ed2.aleja.compresion;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -7,24 +8,53 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+    Toolbar toolbar;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar_navigation);
+        toolbar = findViewById(R.id.toolbar_navigation);
         toolbar.setTitle(R.string.my_comp);
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.nav_open, R.string.nav_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, new CompresosFragment()).commit();
+            navigationView.setCheckedItem(R.id.compresiones);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.compresiones:
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, new CompresosFragment()).commit();
+                toolbar.setTitle(R.string.my_comp);
+                break;
+            case R.id.comprimir:
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, new ComprimirFragment()).commit();
+                toolbar.setTitle(R.string.comprimir);
+                break;
+            case R.id.descomprimir:
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, new DescomprimirFragment()).commit();
+                toolbar.setTitle(R.string.descomprimir);
+                break;
+        }
+        navigationView.setCheckedItem(item.getItemId());
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override

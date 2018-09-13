2a8d3634826197ee89245393b755d936376172af
EzioAARM <alejandrom9712@gmail.com>
2018-09-13 09:24:23 -0600

    Diseño de compresion

--- app/src/main/java/com/ed2/aleja/compresion/ComprimirFragment.java
+++ app/src/main/java/com/ed2/aleja/compresion/ComprimirFragment.java
@@ -1,5 +1,7 @@
 package com.ed2.aleja.compresion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +9,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 public class ComprimirFragment extends Fragment {
+
+    private int valorRetornado = 1;
+    View rootView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.comprimir_fragment, container, false);
+        rootView = inflater.inflate(R.layout.comprimir_fragment, container, false);
+        Button boton = (Button) rootView.findViewById(R.id.examinar_comprimir);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                startActivityForResult(Intent.createChooser(intent, "Seleccionar archivo"), valorRetornado);
+            }
+        });
+        return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        CharSequence texto = "";
+        if (resultCode == RESULT_CANCELED) {
+            texto = "No se selecciono ningún archivo";
+            Toast.makeText(this.getContext(), texto, Toast.LENGTH_SHORT);
+        }
+        if ((resultCode == RESULT_OK) && (requestCode == valorRetornado )) {
+            Uri uri = data.getData();
+            TextView mostrarUbicacion = (TextView) rootView.findViewById(R.id.nombre_archivo);
+            mostrarUbicacion.setText(uri.getPath());
+        }
     }
 }
--- app/src/main/java/com/ed2/aleja/compresion/MainActivity.java
+++ app/src/main/java/com/ed2/aleja/compresion/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ed2.aleja.compresion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,6 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;

97047c6921d32a864859609aa7b37136a27b67b5
EzioAARM <alejandrom9712@gmail.com>
2018-09-19 20:27:34 -0600

    Escribir archivo

--- app/src/main/java/com/ed2/aleja/compresion/DescomprimirFragment.java
+++ app/src/main/java/com/ed2/aleja/compresion/DescomprimirFragment.java
@@ -1,5 +1,6 @@
 package com.ed2.aleja.compresion;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class DescomprimirFragment extends Fragment {
+
+    View rootView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.descomprimir_fragment, container, false);
+        rootView = inflater.inflate(R.layout.descomprimir_fragment, container, false);
+        Button botonPrueba = (Button) rootView.findViewById(R.id.boton_test);
+        botonPrueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri myUri = Uri.parse("http://www.google.com");
+                huffmanCoding huff = new huffmanCoding(myUri, rootView.getContext());
+                try {
+                    huff.escribirArchivoCompreso("archivoPruebaEx", "HolaHolaHola");
+                } catch (Exception e) {
+                    Toast.makeText(rootView.getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        return rootView;
     }
 }
